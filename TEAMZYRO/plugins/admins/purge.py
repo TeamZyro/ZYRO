31da244a727ab87559069e7338be67c74294de37
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-04 02:11:47 +0530

    Update purge.py

--- TEAMZYRO/plugins/admins/purge.py
+++ TEAMZYRO/plugins/admins/purge.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from pyrogram.enums import ChatType
 from pyrogram.errors import MessageDeleteForbidden, RPCError
 from pyrogram.types import Message
-from TEAMZYRO.utils.daxx_ban import admin_filter
+from TEAMZYRO.utils.ZYRO_ban import admin_filter
 from TEAMZYRO import app
 
 
