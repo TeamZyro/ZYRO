129592c90148845dac3b22cad9a753bdadaac2ea
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-04 02:10:35 +0530

    Update unbanall.py

--- TEAMZYRO/plugins/admins/unbanall.py
+++ TEAMZYRO/plugins/admins/unbanall.py
@@ -2,7 +2,7 @@ from TEAMZYRO import app
 from config import OWNER_ID
 from pyrogram import filters, enums
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from TEAMZYRO.utils.daxx_ban import admin_filter
+from TEAMZYRO.utils.ZYRO_ban import admin_filter
 
 BOT_ID = "6974730627"
 
