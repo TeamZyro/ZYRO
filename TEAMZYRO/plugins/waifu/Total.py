79e9ff0781e80e6167c006ea97c8e390cda6eed4
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-04 01:54:57 +0530

    Update and rename Test to Total.py

---
+++ TEAMZYRO/plugins/waifu/Total.py
@@ -0,0 +1,21 @@
+import urllib.request
+from pymongo import ReturnDocument
+import os
+from telegram import Update
+from telegram.ext import CommandHandler, CallbackContext
+
+from TEAMZYRO import application, collection, user_collection
+from gridfs import GridFS 
+from io import BytesIO
+
+
+async def check_total_characters(update: Update, context: CallbackContext) -> None:
+    try:
+        total_characters = await collection.count_documents({})
+        
+        await update.message.reply_text(f"Total number of characters: {total_characters}")
+    except Exception as e:
+        await update.message.reply_text(f"Error occurred: {e}")
+
+
+application.add_handler(CommandHandler("total", check_total_characters))
