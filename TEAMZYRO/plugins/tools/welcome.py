5c71c7c917177a7ee5ae40d9e8e260632cdac60e
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-04 10:06:32 +0530

    Update welcome.py

--- TEAMZYRO/plugins/tools/welcome.py
+++ TEAMZYRO/plugins/tools/welcome.py
@@ -16,7 +16,7 @@ from asyncio import sleep
 from pyrogram import filters, Client, enums
 from pyrogram.enums import ParseMode
 from logging import getLogger
-from TEAMZYRO.utils.daxx_ban import admin_filter
+from TEAMZYRO.utils.ZYRO_ban import admin_filter
 from PIL import ImageDraw, Image, ImageFont, ImageChops
 from pyrogram import *
 from pyrogram.types import *
