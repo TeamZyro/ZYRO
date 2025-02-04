1855861dfd4b98ef2507121151cb0ec62f9b8c9f
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-04 12:50:14 +0530

    Update mustjoin.py

--- TEAMZYRO/plugins/bot/mustjoin.py
+++ TEAMZYRO/plugins/bot/mustjoin.py
@@ -8,7 +8,7 @@ from TEAMZYRO import app
 
 MUST_JOIN = "CARD3DBOTx"
 #------------------------
-@app.on_message(filters.incoming & filters.private, group=-1)
+# @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
     if not MUST_JOIN:
         return
