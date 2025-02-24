6fb0138016123c595031d7b97118f1e8402a8c5b
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-24 16:07:49 +0530

    Update __main__.py

--- TEAMZYRO/__main__.py
+++ TEAMZYRO/__main__.py
@@ -6,12 +6,14 @@ from pytgcalls.exceptions import NoActiveGroupCall
 
 import config
 from TEAMZYRO import LOGGER, app, userbot
-from TEAMZYRO.core.call import ZYRO
+from TEAMZYRO.core.call import ZYRO as DAXX
 from TEAMZYRO.core.application import application
 from TEAMZYRO.misc import sudo
 from TEAMZYRO.plugins import ALL_MODULES
 from TEAMZYRO.utils.database import get_banned_users, get_gbanned
 from config import BANNED_USERS
+
+
 
 async def init():
     if (
@@ -21,9 +23,8 @@ async def init():
         and not config.STRING4
         and not config.STRING5
     ):
-        LOGGER(__name__).error("String session not filled, please fill a Pyrogram session")
+        LOGGER(__name__).error("𝐒𝐭𝐫𝐢𝐧𝐠 𝐒𝐞𝐬𝐬𝐢𝐨𝐧 𝐍𝐨𝐭 𝐅𝐢𝐥𝐥𝐞𝐝, 𝐏𝐥𝐞𝐚𝐬𝐞 𝐅𝐢𝐥𝐥 𝐀 𝐏𝐲𝐫𝐨𝐠𝐫𝐚𝐦 𝐒𝐞𝐬𝐬𝐢𝐨𝐧")
         exit()
-
     await sudo()
     try:
         users = await get_gbanned()
@@ -34,48 +35,47 @@ async def init():
             BANNED_USERS.add(user_id)
     except:
         pass
-
-    # Start Pyrogram bot
     await app.start()
-    
-    # Start Pyrogram userbot
+    for all_module in ALL_MODULES:
+        importlib.import_module("DAXXMUSIC.plugins" + all_module)
+    LOGGER("DAXXMUSIC.plugins").info("𝐀𝐥𝐥 𝐅𝐞𝐚𝐭𝐮𝐫𝐞𝐬 𝐋𝐨𝐚𝐝𝐞𝐝 𝐁𝐚𝐛𝐲🥳...")
     await userbot.start()
-    
-    # Start PyTgCalls
-    await ZYRO.start()
+    await DAXX.start()
 
-    # Start Telegram bot using python-telegram-bot
-    # async def run_application():
-    #     await application.initialize()
-    #     await application.start()
-    #     await application.updater.start_polling()
+    async def run_application():
+        await application.initialize()
+        await application.start()
+        await application.updater.start_polling()
     
-    # asyncio.create_task(run_application())
-
+    asyncio.create_task(run_application())
+    
     try:
-        await ZYRO.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
+        await DAXX.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("DAXXMUSIC").error(
-            "Please start your log group voice chat/channel\n\nDAXX BOT STOPPING..."
+            "𝗣𝗹𝗭 𝗦𝗧𝗔𝗥𝗧 𝗬𝗢𝗨𝗥 𝗟𝗢𝗚 𝗚𝗥𝗢𝗨𝗣 𝗩𝗢𝗜𝗖𝗘𝗖𝗛𝗔𝗧\𝗖𝗛𝗔𝗡𝗡𝗘𝗟\n\n𝗗𝗔𝗫𝗫 𝗕𝗢𝗧 𝗦𝗧𝗢𝗣........"
         )
         exit()
     except:
         pass
-
-    await ZYRO.decorators()
+    await DAXX.decorators()
     LOGGER("DAXXMUSIC").info(
-        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎ MADE BY MR DAXX ☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
+        "╔═════ஜ۩۞۩ஜ════╗\n  ☠︎︎𝗠𝗔𝗗𝗘 𝗕𝗬 𝗠𝗥 𝗗𝗔𝗫𝗫☠︎︎\n╚═════ஜ۩۞۩ஜ════╝"
     )
-    
     await idle()
-
-    # Stop all services before exit
     await app.stop()
     await userbot.stop()
-    await application.stop()
-    LOGGER("DAXXMUSIC").info("Stopped DAXX Music Bot..")
-
+    LOGGER("DAXXMUSIC").info("𝗦𝗧𝗢𝗣 𝗗𝗔𝗫𝗫 𝗠𝗨𝗦𝗜𝗖🎻 𝗕𝗢𝗧..")
 
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(init())
+
+    # Start Telegram bot using python-telegram-bot
+    async def run_application():
+        await application.initialize()
+        await application.start()
+        await application.updater.start_polling()
+    
+    asyncio.create_task(run_application())
+
