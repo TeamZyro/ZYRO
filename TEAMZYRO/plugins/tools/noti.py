245cc9b3dc8f287dcd1ca524d742746b589dd57d
Tᴇᴀᴍ【ＺＹＲＯ】 <harshmanjhi0004@gmail.com>
2025-02-26 07:25:55 +0530

    Update noti.py

--- TEAMZYRO/plugins/tools/noti.py
+++ TEAMZYRO/plugins/tools/noti.py
@@ -1,24 +1,35 @@
-from pyrogram import Client, filters
-from pyrogram.types import ChatMemberUpdated
-from TEAMZYRO import app
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters, CallbackContext
 
-@app.on_chat_member_updated(filters.group)
-async def track_promote_demote(client: Client, chat_member: ChatMemberUpdated):
-    old = chat_member.old_chat_member
-    new = chat_member.new_chat_member
+# Replace these with your own values
+from TEAMZYRO import application
 
-    admin = chat_member.from_user  # The admin who performed the action
-    user = new.user  # The user who was promoted or demoted
+# Function to handle promote, demote, ban, and unban actions
+async def track_actions(update: Update, context: CallbackContext):
+    message = update.message
 
-    if old.status != new.status:  # Check if the status changed
-        if new.status in ["administrator", "owner"] and old.status not in ["administrator", "owner"]:
-            action = "promoted"
-        elif new.status not in ["administrator", "owner"] and old.status in ["administrator", "owner"]:
-            action = "demoted"
-        else:
-            return  # No promotion or demotion happened
+    # Get the user who performed the action (admin)
+    admin = message.from_user
 
-        await client.send_message(
-            chat_member.chat.id,
+    # Get the user who was affected (promoted/demoted/banned/unbanned)
+    user = None
+    if message.new_chat_members:
+        user = message.new_chat_members[0]
+        action = "promoted"
+    elif message.left_chat_member:
+        user = message.left_chat_member
+        action = "banned or removed"
+    else:
+        return
+
+    if user:
+        await message.reply_text(
             f"@{admin.username} has {action} @{user.username}."
         )
+
+
+    # Adding a message handler to track actions
+
+application.add_handler(MessageHandler(filters.ChatType.GROUPS, track_actions))
+
+  
